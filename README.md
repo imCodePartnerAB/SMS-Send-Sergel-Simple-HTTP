6e4ca008d94e4c1cbccbb849de690c79027da503
Eivin Giske Skaaren <eivin@sysmystic.com>
2017-01-21 13:45:59 +0100

    Created CPAN dist

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+SMS::Send::Sergel::Simple::HTTP - Blah blah blah
+
+# SYNOPSIS
+
+    use SMS::Send::Sergel::Simple::HTTP;
+
+# DESCRIPTION
+
+SMS::Send::Sergel::Simple::HTTP is
+
+# AUTHOR
+
+Eivin Giske Skaaren <eivin@sysmystic.com>
+
+# COPYRIGHT
+
+Copyright 2017- Eivin Giske Skaaren
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/SMS/Send/Sergel/Simple/HTTP.pm
@@ -0,0 +1,39 @@
+package SMS::Send::Sergel::Simple::HTTP;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+SMS::Send::Sergel::Simple::HTTP - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use SMS::Send::Sergel::Simple::HTTP;
+
+=head1 DESCRIPTION
+
+SMS::Send::Sergel::Simple::HTTP is
+
+=head1 AUTHOR
+
+Eivin Giske Skaaren E<lt>eivin@sysmystic.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Eivin Giske Skaaren
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
